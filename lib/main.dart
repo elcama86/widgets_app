acf9585fe6281a6a869d041dc4a6cda5630d024d
Hector Montilla <elcama_86@msn.com>
2023-06-13 15:45:36 -0400

    Cards finished

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,10 @@
+import 'dart:io';
 import 'package:flutter/material.dart';
 import 'package:widgets_app/config/router/app_router.dart';
 import 'package:widgets_app/config/theme/app_theme.dart';
 
-void main() {
+void main() async {
+  HttpOverrides.global = MyHttpOverrides();
   runApp(const MainApp());
 }
 
@@ -16,5 +18,13 @@ class MainApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       theme: AppTheme(selectedColor: 6).getTheme(),
     );
+  }
+}
+
+class MyHttpOverrides extends HttpOverrides{
+  @override
+  HttpClient createHttpClient(SecurityContext? context){
+    return super.createHttpClient(context)
+      ..badCertificateCallback = (X509Certificate cert, String host, int port)=> true;
   }
 }
