7f2c3af501b550ed87c64531ec46e683a4676ceb
Hector Montilla <elcama_86@msn.com>
2023-06-13 12:33:30 -0400

    go_router configurado

--- lib/presentation/screens/home/home_screen.dart
+++ lib/presentation/screens/home/home_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
 import 'package:widgets_app/config/menu/menu_items.dart';
 
 class HomeScreen extends StatelessWidget {
@@ -52,7 +53,7 @@ class _CustomListTile extends StatelessWidget {
         color: colors.primary,
       ),
       onTap: () {
-        Navigator.pushNamed(context, item.link);
+        context.push(item.link);
       },
     );
   }
