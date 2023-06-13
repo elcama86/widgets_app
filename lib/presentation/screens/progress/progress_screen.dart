733fc9772b4476f7fcc2e882fba9e1136d1461f6
Hector Montilla <elcama_86@msn.com>
2023-06-13 17:07:03 -0400

    progress indicator finished

--- lib/presentation/screens/progress/progress_screen.dart
+++ lib/presentation/screens/progress/progress_screen.dart
@@ -2,11 +2,87 @@ import 'package:flutter/material.dart';
 
 class ProgressScreen extends StatelessWidget {
   static const String name = 'progress_screen';
-  
+
   const ProgressScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Progress Indicators"),
+      ),
+      body: const _ProgressView(),
+    );
+  }
+}
+
+class _ProgressView extends StatelessWidget {
+  const _ProgressView();
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Column(
+        children: const [
+          SizedBox(
+            height: 30.0,
+          ),
+          Text("Circular Progress Indicator"),
+          SizedBox(
+            height: 10.0,
+          ),
+          CircularProgressIndicator(
+            strokeWidth: 2.0,
+            backgroundColor: Colors.black45,
+          ),
+          SizedBox(
+            height: 20.0,
+          ),
+          Text("Circular y Linear Progress Indicator Controlado"),
+          SizedBox(
+            height: 10.0,
+          ),
+          _ControlledProgressIndicator(),
+        ],
+      ),
+    );
+  }
+}
+
+class _ControlledProgressIndicator extends StatelessWidget {
+  const _ControlledProgressIndicator();
+
+  @override
+  Widget build(BuildContext context) {
+    return StreamBuilder(
+      stream: Stream.periodic(const Duration(milliseconds: 300), (value) {
+        return (value * 2) / 100;
+      }).takeWhile((value) => value < 100),
+      builder: (context, snapshot) {
+        final double progressValue = snapshot.data ?? 0.0;
+        
+        return Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 20.0),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              CircularProgressIndicator(
+                value: progressValue,
+                strokeWidth: 2.0,
+                backgroundColor: Colors.black12,
+              ),
+              const SizedBox(
+                width: 20.0,
+              ),
+              Expanded(
+                child: LinearProgressIndicator(
+                  value: progressValue,
+                ),
+              ),
+            ],
+          ),
+        );
+      },
+    );
   }
 }
