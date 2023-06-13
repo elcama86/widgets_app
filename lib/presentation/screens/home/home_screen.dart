a7ee8972a8f35df2c7a413fa5b97d46ee8327f35
Hector Montilla <elcama_86@msn.com>
2023-06-13 13:28:34 -0400

    ButtonsScreen finished

--- lib/presentation/screens/home/home_screen.dart
+++ lib/presentation/screens/home/home_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
 import 'package:widgets_app/config/menu/menu_items.dart';
+import 'package:widgets_app/presentation/screens/cards/cards_screen.dart';
 
 class HomeScreen extends StatelessWidget {
   static const String name = 'home_screen';
@@ -55,6 +56,7 @@ class _CustomListTile extends StatelessWidget {
         color: colors.primary,
       ),
       onTap: () {
+        // context.pushNamed(CardsScreen.name);
         context.push(item.link);
       },
     );
