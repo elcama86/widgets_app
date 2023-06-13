1da9364229e5e4d2fae56019066ea0fabc7b29c6
Hector Montilla <elcama_86@msn.com>
2023-06-13 11:10:23 -0400

    Home screen implementado

--- lib/presentation/screens/home/home_screen.dart
+++ lib/presentation/screens/home/home_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:widgets_app/config/menu/menu_items.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
@@ -9,6 +10,50 @@ class HomeScreen extends StatelessWidget {
       appBar: AppBar(
         title: const Text("Flutter + Material 3"),
       ),
+      body: const _HomeView(),
+    );
+  }
+}
+
+class _HomeView extends StatelessWidget {
+  const _HomeView();
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView.builder(
+      itemCount: appMenuItems.length,
+      itemBuilder: (context, index) {
+        final item = appMenuItems[index];
+        return _CustomListTile(item: item);
+      },
+    );
+  }
+}
+
+class _CustomListTile extends StatelessWidget {
+  final MenuItem item;
+
+  const _CustomListTile({
+    required this.item,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    final colors = Theme.of(context).colorScheme;
+    return ListTile(
+      leading: Icon(
+        item.icon,
+        color: colors.primary,
+      ),
+      title: Text(item.title),
+      subtitle: Text(item.subTitle),
+      trailing: Icon(
+        Icons.arrow_forward_ios_rounded,
+        color: colors.primary,
+      ),
+      onTap: (){
+        //TODO: navegar a otra pantalla
+      },
     );
   }
 }
