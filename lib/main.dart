10cdd57de6b031a9c85094fd71c58befd06bef8b
Hector Montilla <elcama_86@msn.com>
2023-06-15 14:59:29 -0400

    theme change finished

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:widgets_app/config/router/app_router.dart';
 import 'package:widgets_app/config/theme/app_theme.dart';
+import 'package:widgets_app/presentation/providers/theme_provider.dart';
 
 void main() async {
   HttpOverrides.global = MyHttpOverrides();
@@ -13,16 +14,19 @@ void main() async {
   );
 }
 
-class MainApp extends StatelessWidget {
+class MainApp extends ConsumerWidget {
   const MainApp({super.key});
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
+    final int selectedColor = ref.watch(selectedColorProvider);
+    final bool isDarkMode = ref.watch(isDarkModeProvider);
+
     return MaterialApp.router(
       title: "Flutter Widgets",
       routerConfig: appRouter,
       debugShowCheckedModeBanner: false,
-      theme: AppTheme(selectedColor: 3).getTheme(),
+      theme: AppTheme(selectedColor: selectedColor, isDarkMode: isDarkMode).getTheme(),
     );
   }
 }
--- lib/presentation/providers/theme_provider.dart
+++ lib/presentation/providers/theme_provider.dart
@@ -1,3 +1,14 @@
+import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:widgets_app/config/theme/app_theme.dart';
 
+// Listado de colores inmutables
+final colorListProvider = Provider<List<Color>>((ref) => colorList);
+
+// Un simple booleano
 final isDarkModeProvider = StateProvider<bool>((ref) => false);
+
+// Un simple int
+final selectedColorProvider = StateProvider<int>((ref) => 0);
+
+
